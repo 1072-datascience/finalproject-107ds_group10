38b7f721bd8022a4dbdf06591a4428049937efa7
caneko <gth.cf8d2@gmail.com>
2019-05-18 23:02:06 +0800

    rm unused variables, filter OCCUPATION first letter, transform BUDGET into quantile

--- 02FeatureEngineering.R
+++ 02FeatureEngineering.R
@@ -14,7 +14,7 @@ nfolds <- 10
 for (y in Y){
   dTrAll$folds[dTrAll$BUY  == y] <-  cut(seq(1,sum(dTrAll$BUY == y)),breaks=nfolds,labels=FALSE)
 }
-
+rm(y,Y)
 
 #TODO: for loop to CV
 library(data.table)
@@ -49,6 +49,8 @@ tmp[tmp > Threshold] <- Threshold
 tmp[tmp < -Threshold] <- -Threshold
 dTrainFix$WEIGHT <- tmp
 
+rm(tmp,Threshold)
+
 library(gridExtra)
 plotH_B <- ggplot(dTrain[!is.na(dTrain$HEIGHT),],aes(x = HEIGHT)) +
   geom_histogram(aes(y=..density..), colour="black", fill="white") +
@@ -58,6 +60,8 @@ plotH_A <- ggplot(dTrainFix,aes(x = HEIGHT)) +
   geom_density(alpha=.2, fill="#FF6666",adjust = 2) + labs(x = 'HEIGHT-After')
 grid.arrange(plotH_B, plotH_A, nrow=2, ncol=1)
 
+rm(plotH_B,plotH_A)
+
 plotW_B <- ggplot(dTrain[!is.na(dTrain$WEIGHT),],aes(x = WEIGHT)) +
   geom_histogram(aes(y=..density..), colour="black", fill="white") +
   geom_density(alpha=.2, fill="#FF6666",adjust = 2) + labs(x = 'WEIGHT-Before')
@@ -66,8 +70,46 @@ plotW_A <- ggplot(dTrainFix,aes(x = WEIGHT)) +
   geom_density(alpha=.2, fill="#FF6666",adjust = 2) + labs(x = 'WEIGHT-After')
 grid.arrange(plotW_B, plotW_A, nrow=2, ncol=1)
 
+rm(plotW_B,plotW_A)
+
 # pdf('img/data/NAfix/HEIGHT-After.pdf')
 # dev.off()
+
+# OCCUPATION filter
+dTrainFix$OCCUPATION <- sapply(as.character(dTrain$OCCUPATION), function(x) strsplit(x, NULL)[[1]][1])
+
+ggplot(dTrainFix, aes(x=OCCUPATION ,fill = factor(BUY_TYPE))) +
+  geom_bar(stat='count', position='dodge') + labs(x = 'OCCUPATION' ) +
+  facet_wrap(~factor(BUY_TYPE)) + theme_few()
+
+save.image('02.RData')
+
+rm(list=ls());load('02.RData');cat('\f')
+#Budget Quantilization
+library('dataPreparation')
+BUDGETQUANTILEmodel <- build_bins(dTrainFix,col='BUDGET',n_bins = 10,type = 'equal_freq', verbose = TRUE)
+tmp <- fastDiscretization(dTrainFix, bins = BUDGETQUANTILEmodel, verbose = F)
+dTrainFix$BUDGET_QUAN <- tmp$BUDGET
+rm(tmp)
+
+library('tidyverse')
+dTrainFix$BUDGET_QUAN <- fct_rev(dTrainFix$BUDGET_QUAN)
+
+plotBF <- ggplot(dTrainFix, aes(x=BUDGET_QUAN ,fill = factor(BUY_TYPE))) +
+  geom_bar(stat='count', position='dodge') + labs(x = 'BUDGET_QUAN' ) +
+  facet_wrap(~factor(BUY_TYPE),scales='fixed') + theme_few() + 
+  theme(axis.text.x = element_text(size=12)) +  ggtitle('Fixed Y') +
+  scale_x_discrete(labels = 1:10) 
+
+plotBR <- ggplot(dTrainFix, aes(x=BUDGET_QUAN ,fill = factor(BUY_TYPE))) +
+  geom_bar(stat='count', position='dodge') + labs(x = 'BUDGET_QUAN' ) +
+  facet_wrap(~factor(BUY_TYPE),scales='free') + theme_few() + 
+  theme(axis.text.x = element_text(size=12)) +  ggtitle('Relative Y') +
+  scale_x_discrete(labels = 1:10)
+
+grid.arrange(plotBF, plotBR, nrow=2, ncol=1)
+
+rm(plotBF, plotBR)
 
 #Replace NA with 'NA' in catagorical columns
 Var_Cat_NA <-  colnames(dTrainFix)[(sapply(dTrainFix,function(x) sum(is.na(x))) > 0)]
@@ -76,7 +118,6 @@ for (c in Var_Cat_NA){
   #tmp2 <- factor(ifelse(is.na(tmp), 'NA', paste(tmp)), levels = c(levels(tmp), 'NA'))
 }
 #Dummy Var
-
-library('dataPreparation')
+Var_Cat <- c(Var_Cat,'BUDGET_QUAN')
 OHEmodel <- build_encoding(dTrainFix, cols = Var_Cat, verbose = TRUE)
 dTrainFix_OHE <- one_hot_encoder(dTrainFix, encoding = OHEmodel, drop = TRUE)
